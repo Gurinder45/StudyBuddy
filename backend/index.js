ae4954dab07ee415fb95133f2b05ccb34f6abc04
ksa138 <ksa138@sfu.ca>
2023-03-10 15:48:58 -0800

    database connection not working, tells me i am unauthorized

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,48 @@
-const e = require('express')
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
-mongoose.connect("mongodb://cmpt372:TimeToStudy@34.170.98.49:27017", {
-    useNewUrlParser:true, useUnifiedTopology:true
-},(err)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            console.log("Successfully connected")
-        }
-    }
-)
+const uri = "mongodb://cmpt372:TimeToStudy@34.170.98.49:27017"
+// let MongoClient = require('mongodb').
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(uri,{useNewUrlParser: true})
+var db = mongoose.connection
+console.log("AFTER CONNECTING DB", db)
+db.on('error', console.error.bind(console, "connection error"))
+
+// 
 
 
 
-app.listen(3000, ()=>{
-    console.log("on port 3000")
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    uname: {type: String},
+    password: {type:String, minlength: 2}
 })
+
+var User = mongoose.model("user", userSchema)
+
+var createUser = async() =>{
+    var bob = new User({
+        uname: "bob",
+        password: "12"
+    })
+
+    try{
+        
+        await bob.save()
+        console.log('done!')
+    }
+    catch(e){
+        console.log("SORRY")
+        console.log(e.codeName)
+    }
+    process.exit()
+}
+
+createUser()
 
 //admin:WebDevIsFun@34.170.98.49:27017/?authMechanism=DEFAULT
