076e4574942ea4a817bb50938d7d97d533abde88
ksa138 <ksa138@sfu.ca>
2023-03-14 14:15:28 -0700

    connected and can post to db

--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,13 @@
-// const express = require('express')
-// const mongoose = require('mongoose')
-// const app = express()
-// const cors = require('cors')
 
-// app.use(cors())
-// app.use(express.json())
-
-// const uri = "mongodb://cmpt372:TimeToStudy@34.170.98.49:27017/studybuddy?authSource=admin"
-// mongoose.set('strictQuery', true);
-// mongoose.connect(uri,{useNewUrlParser: true})
-// var db = mongoose.connection
-// db.on('error', console.error.bind(console, "connection error"))
-
-// var Schema = mongoose.Schema;
-
-// var userSchema = new Schema({
-//     uname: {type: String},
-//     password: {type:String, minlength: 2}
-// })
-
-// var User = mongoose.model("user", userSchema)
-
-
-
-// app.post('localhost:3000/sendLogin', (req, res)=>{
-//     console.log("next is the req body")
-//     console.log(req.body)
-//     res.json({status:"ok"})
-// })
-
-// app.get('/hello', (req,res)=>{
-//     res.send('welcome!!')
-// })
-
-// app.listen(1234, ()=>{
-//     console.log('** Server Starting **')
-// })
-
-
+const express = require('express')
+const app = express()
 const mongoose = require('mongoose')
 const User = require("./user.model")
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 mongoose.set('strictQuery', false);
 const uri = 'mongodb://cmpt372:TimeToStudy@34.170.98.49:27017/studybuddy?authSource=admin'
@@ -54,36 +22,47 @@ mongoose.connect(uri, connectParams).then(()=> {
     console.log("ERROR: ", e);
 })
 
-//only local copy in js program
-// const user = new User()
-// user.uname = "katie";
-// user.password = 12345;
-// user.save((err, savedJob)=> {
-//     if (err){
-//         console.log(err);
-//     }
-//     else{
-//         console.log(savedJob);
-//     }
-// })
+app.get('/', (req,res)=>{
+    console.log("reached");
+    res.json(200);
+})
 
-var createUser = async() =>{
-    var bob = new User({
-        uname: "bob",
-        password: "12"
-    })
 
-    try{
+// var createUser = async() =>{
+//     var bob = new User({
+//         uname: "bob",
+//         password: "12"
+//     })
+
+//     try{
         
-        await bob.save()
-        console.log('done!')
-    }
-    catch(e){
-        console.log("SORRY")
-        console.log(e.codeName)
-    }
-    process.exit()
-}
+//         await bob.save()
+//         console.log('done!')
+//     }
+//     catch(e){
+//         console.log("SORRY")
+//         console.log(e.codeName)
+//     }
+//     process.exit()
+// }
 
-createUser()
+// createUser()
+
+
+app.post('/sendLogin', async (req, res)=>{
+    console.log("next is the req body")
+    let { uname, password } = req.body;
+    console.log(uname, password)
+    res.status(200).json(req.body);
+    let newU = new User;
+    newU.uname = uname;
+    newU.password = password;
+    await newU.save();
+    // res.json(req.body)
+    // res.json({status:"ok"})
+})
+
+app.listen(1234, ()=>{
+    console.log('** Server Starting **')
+})
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [uname, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function registerUser(event:any){
+    event.preventDefault()
+    const response = await fetch('http://localhost:1234/sendLogin', {
+      method:'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        uname: uname.trim(),
+        password: password.trim()
+      })
+    });
+
+    const data = await response.json()
+
+    console.log(" THIS IS THE DATA", data)
+    setName(data.uname)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p> Hello {uname}</p>
+      <form onSubmit={registerUser}>
+        <input value={uname} type="text" name="uname" onChange={(e)=>{setName(e.target.value)}} placeholder='username' />
+        <input value={password} type="password" name="password" onChange={(e)=>{setPassword(e.target.value)}} placeholder='password'/>
+        <input type="submit" value="sign-in" />
+      </form>
     </div>
   );
 }
