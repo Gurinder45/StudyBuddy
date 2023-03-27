43879639ed1b0aa5f20dd16deedca2ef01b15a6d
gparent <greg_parent@sfu.ca>
2023-03-27 12:49:04 -0700

    Add schema comments

--- backend/chatroom.model.js
+++ backend/chatroom.model.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose')
 
 var Schema = mongoose.Schema;
 
-// Chatroom model will keep track of everything needed for 
+// Chatroom model will keep track of everything needed for "study sessions"
+// id: to keep track of the unique chat
+// title: a user given title for the study session 
+// users: an array of users that are in the study session
 var chatroomSchema = new Schema({
     id: {type: String, required: true},
-    users: {type: [String], required: true}
+    title: {type: String, required: true},
+    users: {type: [String], required: true},
 })
 
 module.exports = mongoose.model("chatroom", chatroomSchema)
